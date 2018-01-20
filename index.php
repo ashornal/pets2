c7d7d7960bd1fd4ecb1e2fc5ed03d53b5e544930
Alex Shornal <shornality@yahoo.com>
2018-01-20 00:24:06 -0500

    index done

--- index.php
+++ index.php
@@ -1,20 +1,42 @@
 <?php
+session_start();
 
-//Require the autoload file
-require_once('vendor/autoload.php');
 
-//Create an instance of the Base Class
+//require the autoload file
+require_once ('vendor/autoload.php');
+
+
+//create an instance of the Base class
 $f3 = Base::instance();
 
-//Define a default route
-$f3->route('GET /', function()
-{
 
-    $view = new View;
-    echo $view->render
-    ('views/home.html');
+//define a default route
+$f3->route("GET /", function()
+{
+    $template = new Template();
+    echo $template->render('views/home.html');
+}
+);
+$f3->route("GET /pets/order", function()
+{
+    $template = new Template();
+    echo $template->render('views/form1.html');
+}
+);
+$f3->route("POST /pets/order2", function()
+{
+    $template = new Template();
+    echo $template->render('views/form2.html');
+    $_SESSION['animal'] = $_POST['animal'];
+}
+);
+$f3->route("POST /pets/results", function()
+{
+    $_SESSION['color'] = $_POST['color'];
+    echo "<h1>Results Page</h1>";
+    echo "Thank you for ordering a ".$_SESSION['color']. " ".$_SESSION['animal'];
 }
 );
 
-//Run Fat-Free
+//run Fat-Free
 $f3->run();
