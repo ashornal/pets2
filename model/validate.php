22cf0ec0f5bc7ca57bad58022a8afe8a807bdc7d
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-01-26 15:29:57 -0500

    modified validate.php

--- model/validate.php
+++ model/validate.php
@@ -1,19 +1,29 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
 function validColor($color)
 {
     global $f3;
     return in_array($color, $f3->get('colors'));
 }
+
+
 function validString($string)
 {
     return $string != null && ctype_alpha($string);
 }
 
 $errors = array();
-if(!validColor())
+
+if(!validColor($string))
 {
     $errors['color'] = "Please enter a valid color";
+}
+
+if(!validString($string))
+{
+    $errors['name'] = "Please enter a valid name";
+    $errors['type'] = "Please enter a valid type";
 }
 $success = sizeof($errors) == 0;
