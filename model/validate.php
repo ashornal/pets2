f9626bdb0aceef6c8a6f7c175190996b4b3f1a23
Alex Shornal <shornality@yahoo.com>
2018-01-26 14:16:03 -0500

    added validate page

---
+++ model/validate.php
@@ -0,0 +1,16 @@
+<?php
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
+function validString($string)
+{
+    return $string != null && ctype_alpha($string);
+}
+$errors = array();
+if(!validColor())
+{
+    $errors['color'] = "Please enter a valid color";
+}
+$success = sizeof($errors) == 0;
